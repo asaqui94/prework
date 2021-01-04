954c2ff8767f277d6a16c60e21c74904ab1987ee
asaqui94 <arpitasaqui@gmail.com>
2021-01-04 16:35:16 -0500

    q4 change

--- q4.py
+++ q4.py
@@ -1,6 +1,6 @@
 def is_leap_year(a_year):
 	print(f'\n{str(a_year)} is a leap year.')
-	if a_year%4 == 0:
+	if a_year%4 == 0 and (a_year%400 == 0 or a_year%100 !=0)
 		print('True')
 	else: 
 		print('False')
